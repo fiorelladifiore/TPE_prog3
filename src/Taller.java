185a9c709f6dc437ed183a044b4a0196035d0250
Gabriel Marrero <gab.bmar.05@gmail.com>
2025-11-10 16:50:47 -0300

    agrego explicacion estrategia greedy

--- src/Taller.java
+++ src/Taller.java
@@ -59,6 +59,30 @@ public class Taller {
     }
 
     /** GREEDY -------------------------------------------------------------------------------------------- */
+    /**
+     * Estrategia para la construcción de piezas:
+     * 
+     * - Candidatos:
+     *      Cada máquina disponible representa un candidato posible para ser
+     *      utilizado en la construcción de las piezas.
+     * 
+     * - Estrategia de selección:
+     *      Se ordenan las máquinas en orden descendente según su capacidad de
+     *      producción.
+     *      Se selecciona primero las máquinas de mayor capacidad, ya que permite
+     *      alcanzar el objetivo con la menor cantidad de encendidos posibles.
+     * 
+     * - Criterio de aceptación:
+     *      Mientras la suma de piezas construidas no exceda la cantidad total
+     *      requerida, se enciende la máquina actual tantas veces como sea posible.
+     *      Luego se continua con la siguiente máquina del listado.
+     * 
+     * - Consideraciones respecto a encontrar o no solución:
+     *      Si la suma total de piezas producidas es exactamente igual a la
+     *      requerida, se considera que se encontró una solución posible.
+     *      En caso contrario, si no es posible alcanzar exactamente el total
+     *      solicitado, no se devuelve solución (retorna null).
+     */
     public Solucion construirPiezasGreedy(List<Maquina> maquinasList, int totalPiezas) {
         Collections.sort(maquinasList);
 
